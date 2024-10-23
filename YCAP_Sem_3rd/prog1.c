308b9520dc7ede3b2c3d0c0d30720ef7c2755017
Yash Izate <yashizate.softech@gmail.com>
2024-10-23 14:56:26 +0530

    y-cap 3rd sem

---
+++ YCAP_Sem_3rd/prog1.c
@@ -0,0 +1,40 @@
+// #include <stdio.h>
+
+// int main() {
+//   const int height = 100;
+//   const float num = 3.14;
+//   const char letter = 'A';
+//   const char ch[] = "ABC";
+//   const char backlash = '?';
+
+//   printf("********Yash Izate********\n");
+//   printf("Value of height: %d\n", height);
+//   printf("Value of Number: %f\n", num);
+//   printf("Value of Letter: %c\n", letter);
+//   printf("Value of letter_sequence: %s\n", ch);
+//   printf("Value of backlash_char: %c\n", backlash);
+
+//   return 0;
+
+// }
+
+
+// #include <stdio.h>
+// int main() {
+//   int a=1, b=2;
+//   {
+//     a = a+b;
+//     int a=10;
+//     a=a+b;
+//   }
+
+//   printf("%d", a);
+// }
+
+
+#include <stdio.h>
+int main()
+{
+  printf("%d", printf("%d\n", 1) +1);
+  return 0;
+}
