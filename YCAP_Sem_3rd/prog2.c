63ffc01be43d872865709c09456a7c7a73676350
Yash Izate <yashizate.softech@gmail.com>
2024-10-24 16:19:59 +0530

    update

--- YCAP_Sem_3rd/prog2.c
+++ YCAP_Sem_3rd/prog2.c
@@ -1,1 +1,26 @@
-S
+//Name: Yash Izate
+//Roll No: 61
+
+#include<stdio.h>
+
+void swap(int*,int*);
+
+int main(){
+  int num1,num2;
+  printf("input 1st number:");
+  scanf("%d",&num1);
+  printf("input 2nd number:");
+  scanf("%d",&num2);
+  printf("before swapping ;number1=%d,number2=%d",num1,num2);
+  swap(&num1,&num2);
+  printf("\n after swapping :number1=%d,number2=%d \n \n",num1,num2);
+  return 0;
+}
+
+void swap(int *p,int *q)
+{  
+  int tmp;
+  tmp=*p;
+  *p=*q;
+  *q=tmp;
+}
