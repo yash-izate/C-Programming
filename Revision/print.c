281a413133e1d1e3fc188bb0212fe014a9c86795
Yash Izate <yashizate.softech@gmail.com>
2025-09-24 05:14:30 +0530

    added print code

--- Revision/print.c
+++ Revision/print.c
@@ -5,7 +5,8 @@ int main()
 
     printf("Hello World!\n");
     printf("My name is %c\n", 'A');
-    printf("My age is %d", 22);
+    printf("My age is %d\n", 22);
+    printf("My weight is %f", 78.8);
 
     return 0;
 }
